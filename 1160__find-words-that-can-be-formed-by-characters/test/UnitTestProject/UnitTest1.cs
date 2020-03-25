d4dc5100e58e7669683f950f6f41242d2930240d
Cheng Zhang <zhangcheng81@gmail.com>
2020-03-25 22:51:00 +0800

    rename & use data driven tests

--- 1160__find-words-that-can-be-formed-by-characters/test/UnitTestProject/UnitTest1.cs
+++ 1160__find-words-that-can-be-formed-by-characters/test/UnitTestProject/UnitTest1.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject
 {
-    [TestClass()]
+    [TestClass]
     public class UnitTest1
     {
         /*
@@ -11,68 +11,21 @@ namespace UnitTestProject
          * 输出：6
          * 解释： 
          * 可以形成字符串 "cat" 和 "hat"，所以答案是 3 + 3 = 6。
-        */
-        [TestMethod()]
-        public void TestMethod1()
-        {
-            var words = new string[] { "cat", "bt", "hat", "tree" };
-            var chars = "atach";
-            var expected = 6;
-
-            Solution sut = new Solution();
-            var actual = sut.CountCharacters(words, chars);
-            Assert.AreEqual(expected, actual);
-        }
-
-        /*
+        
          * 输入：words = ["hello","world","leetcode"], chars = "welldonehoneyr"
          * 输出：10
          * 解释：
          * 可以形成字符串 "hello" 和 "world"，所以答案是 5 + 5 = 10。
          */
-        [TestMethod()]
-        public void TestMethod2()
+
+        [TestMethod]
+        [DataRow(new string[] { "hello", "world", "leetcode" }, "", 0)]
+        [DataRow(new string[] { "hello", "world", "leetcode" }, "welldonehoneyr", 10)]
+        [DataRow(new string[] { "cat", "bt", "hat", "tree" }, "atach", 6)]
+        [DataRow(new string[] { }, "", 0)]
+        [DataRow(new string[] { }, "starwar", 0)]
+        public void TestMethod3(string[] words, string chars, int expected)
         {
-            var words = new string[] { "hello", "world", "leetcode" };
-            var chars = "welldonehoneyr";
-            var expected = 10;
-
-            Solution sut = new Solution();
-            var actual = sut.CountCharacters(words, chars);
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod()]
-        public void TestMethod3()
-        {
-            var words = new string[] { "hello", "world", "leetcode" };
-            var chars = "";
-            var expected = 0;
-
-            Solution sut = new Solution();
-            var actual = sut.CountCharacters(words, chars);
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod()]
-        public void TestMethod4()
-        {
-            var words = new string[] { };
-            var chars = "";
-            var expected = 0;
-
-            Solution sut = new Solution();
-            var actual = sut.CountCharacters(words, chars);
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod()]
-        public void TestMethod5()
-        {
-            var words = new string[] { };
-            var chars = "starwar";
-            var expected = 0;
-
             Solution sut = new Solution();
             var actual = sut.CountCharacters(words, chars);
             Assert.AreEqual(expected, actual);
--- compress-string-lcci/test/compress-string-lcci-test/CompressStringTests.cs
+++ compress-string-lcci/test/compress-string-lcci-test/CompressStringTests.cs
@@ -3,37 +3,15 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace compress_string_lcci_test
 {
-    [TestClass()]
+    [TestClass]
     public class CompressStringTests
     {
-        [TestMethod()]
-        public void TestMethod1()
+        [TestMethod]
+        [DataRow("aabcccccaa", "a2b1c5a2")]
+        [DataRow("abbccd", "abbccd")]
+        [DataRow("bb", "bb")]
+        public void TestMethod(string S, string expected)
         {
-            var S = "aabcccccaa";
-            var expected = "a2b1c5a2";
-
-            var sut = new Solution();
-            var actual = sut.CompressString(S);
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod()]
-        public void TestMethod2()
-        {
-            var S = "abbccd";
-            var expected = "abbccd";
-
-            var sut = new Solution();
-            var actual = sut.CompressString(S);
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod()]
-        public void TestMethod3()
-        {
-            var S = "bb";
-            var expected = "bb";
-
             var sut = new Solution();
             var actual = sut.CompressString(S);
             Assert.AreEqual(expected, actual);
