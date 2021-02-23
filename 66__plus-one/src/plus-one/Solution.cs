9b4c4b606ad2b353e66c8e5d87c31255b65e0ff5
张成 <zhangcheng04@megvii.com>
2021-02-23 13:36:26 +0800

    66

--- 66__plus-one/src/plus-one/Solution.cs
+++ 66__plus-one/src/plus-one/Solution.cs
@@ -6,6 +6,16 @@ namespace plus_one
     {
         public int[] PlusOne(int[] digits)
         {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                digits[i]++;
+                if(digits[i] % 10 !=0)
+                {
+                    return digits;
+                }
+            }
+            digits = new int[digits.Length + 1];
+            digits[0] = 1;
             return digits;
         }
     }
--- 66__plus-one/test/plus-one-tests/UnitTest1.cs
+++ 66__plus-one/test/plus-one-tests/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace plus_one_tests
         [DataRow(new int[] { 1, 2, 3 }, new int[] { 1, 2, 4 })]
         [DataRow(new int[] { 0 }, new int[] { 1 })]
         [DataRow(new int[] { 9 }, new int[] { 1, 0 })]
+        [DataRow(new int[] { 9, 9 }, new int[] { 1, 0, 0 })]
         [TestMethod]
         public void TestMethod1(int[] nums, int[] expected)
         {
